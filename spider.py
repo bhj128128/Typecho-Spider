20ab7a0313b7f40242be21c87d6c79846e70f539
wmathor <739616037@qq.com>
2021-10-05 13:44:12 +0800

    update

--- spider.py
+++ spider.py
@@ -2,12 +2,12 @@ import re, os
 import requests
 from bs4 import BeautifulSoup
 from bs4.element import Tag
+from tqdm import tqdm
 
 url = 'https://wmathor.com/admin/manage-posts.php'
-folder_path="D:\\blog\\"    
-cookies = {
-    'cookie':'__gads=ID=11ae0698092d435f-22d563a103c30006:T=1597577131:RT=1597577131:S=ALNI_MaWofxAYcYAZYHwqak3V7b9v3exmw; PHPSESSID=kh181pufibbbk3pml928ir7qt9; 42dba681692dfe704cefb5aef6745f7e__post_views=17%2C1498%2C756%2C754%2C1176%2C1436; 42dba681692dfe704cefb5aef6745f7e__typecho_uid=1; 42dba681692dfe704cefb5aef6745f7e__typecho_authCode=%24T%24V2HjYwClXf5a4af603343261fdf44ae6578db6939',
-}
+folder_path = "D:\\blog\\"
+Cookie = "PHPSESSID=il3p0vrn4mv8b038m1d293e2ei; 42dba681692dfe704cefb5aef6745f7e__post_views=1586%2C1531%2C754%2C1580%2C954%2C1451%2C1581%2C1585%2C1562%2C1529%2C1526%2C1491%2C1455; 42dba681692dfe704cefb5aef6745f7e__typecho_uid=1; 42dba681692dfe704cefb5aef6745f7e__typecho_authCode=%24T%24UcBF16dOheb8885a2a1b135c6979367b21b9578ad"
+cookies = {item.split('=')[0]: item.split('=')[1] for item in Cookie.split('; ')}
 
 def mkdir(path):
     '''Create a folder
@@ -17,6 +17,7 @@ def mkdir(path):
     Returns:
         None
     '''
+
     try:
         folder = os.path.exists(path)
         os.makedirs(path) # makedirs 创建文件时如果路径不存在会创建这个路径
@@ -31,8 +32,8 @@ def check(name):
     Returns:
         Filtering strings after special characters
     '''
-    return name.replace('\\', ' ').replace('/', ' ').replace(':', ' ').replace('*', ' ').replace('?', ' ').replace('<', ' ').replace('>', ' ').replace('|', ' ')
-
+    
+    return name.replace('\\', ' ').replace('/', ' ').replace(':', ' ').replace('*', ' ').replace('?', ' ').replace('<', ' ').replace('>', ' ').replace('|', ' ').replace('"', ' ')
 
 def save(content, article_title, article_category):
     '''Save content to a file. The file name is article_tile.md
@@ -44,6 +45,7 @@ def save(content, article_title, article_category):
     Returns:
         None
     '''
+
     save_name = folder_path + check(article_category) + '\\' + check(article_title) + '.md'
     with open(save_name, 'w', encoding='utf-8') as f:
         f.write(content)
@@ -58,26 +60,29 @@ def get_articles(new_url, session):
     Returns:
         None
     '''
-    # get article information
+
+    # get article information (url, title, category)
     response = session.get(new_url)
     soup = BeautifulSoup(response.text, 'lxml')
     tr_iter = soup.tbody.tr.next_siblings
+
     for tr in tr_iter:
         if isinstance(tr, Tag):
-            # print(tr.contents)
-            article_url = tr.contents[5].a['href']
-            article_title = tr.contents[5].a.contents[0]
-            article_category = tr.contents[-4].a.contents[0]
-
+            try:
+                article_url = tr.contents[5].a['href']
+                article_title = tr.contents[5].a.contents[0]
+                article_category = tr.contents[-4].a.contents[0]
+            except:
+                print('文章' + '"' + article_title + '"' + '有误，请手动检查。提示：检查文章是否没有设置分类')
             # get article content
             response = session.get(article_url)
             soup = BeautifulSoup(response.text, 'lxml')
+            # print(tr)
             if len(soup.textarea.contents) != 0:
                 content = str(soup.textarea.contents[0]).replace('\n', '')
             else:
                 content = ' '
             save(content, article_title, article_category)
-    
 
 def main(url, page_number_start=1, page_number_end=1):
     '''
@@ -105,10 +110,9 @@ def main(url, page_number_start=1, page_number_end=1):
     print('---- Folder Creation Complete ----')
     
     # get all articles
-    for page in range(page_number_start, page_number_end + 1):
+    for page in tqdm(range(page_number_start, page_number_end + 1)):
         new_url = url + '?page=' + str(page)
         get_articles(new_url, session)
-        print('Page {} Complete'.format(page))
 
 if __name__ == '__main__':
-    main(url, page_number_start=1, page_number_end=34)
+    main(url, page_number_start=1, page_number_end=37)
